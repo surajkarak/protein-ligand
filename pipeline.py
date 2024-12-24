a8468a0af274e92b3d6bfedc8f50ed9c519ab2b2
surajkarak <karakulath.suraj@gmail.com>
2024-12-24 22:05:13 +0100

    UPDATE

--- pipeline.py
+++ pipeline.py
@@ -11,8 +11,13 @@ import pickle
 from sklearn.metrics import jaccard_score
 from scipy.spatial.distance import pdist, squareform
 from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import MeanShift
+from sklearn.cluster import OPTICS
 from sklearn.cluster import DBSCAN
 from sklearn.manifold import TSNE
+
+
 
 folder_path = '/Users/surajkwork/Documents/Projects/ProteinLigand/protein-ligand/protein-ligand/BindingSiteAnalysis/kras_md_sites_1'   
 all_binding_sites = []
@@ -47,7 +52,7 @@ def extraction(folder_path):
 
 # Based on residue set intersections
 
-def residue_overlap_distance(site1, site2):
+def residue_overlap_distance(site1, site2, distancetype):
     residues1 = set(site1.get('residues', []))
     residues2 = set(site2.get('residues', []))
     
@@ -57,9 +62,9 @@ def residue_overlap_distance(site1, site2):
     # Jaccard distance = 1 - Jaccard similarity
     return 1 - len(residues1 & residues2) / len(residues1 | residues2)
 
-def pairwise_distances_with_library(sites, distance_func):
+def pairwise_distances_with_library(sites, distance_func, distancetype):
     def distance_wrapper(i, j):
-        return distance_func(sites[int(i[0])], sites[int(j[0])])
+        return distance_func(sites[int(i[0])], sites[int(j[0])], distancetype)
     
     indices = list(range(len(sites)))
     condensed_matrix = pdist([[i] for i in indices], metric=lambda i, j: distance_wrapper(i, j))
@@ -71,7 +76,7 @@ distance_matrix = pairwise_distances_with_library(subset, residue_overlap_distan
 
 # Based on residue scores
 
-def residue_score_distance(site1, site2):
+def residue_score_distance(site1, site2, distancetype):
     res_scores_1 = site1.get('residue_scores', {})
     res_scores_2 = site2.get('residue_scores', {})
     rnames = list(set.union(set(res_scores_1.keys()), set(res_scores_2.keys())))
@@ -79,21 +84,22 @@ def residue_score_distance(site1, site2):
     res_scores_2 = {**dict.fromkeys(rnames, 0), **res_scores_2}
     res_scores_1 = np.asarray(list(res_scores_1.values()), np.float32)
     res_scores_2 = np.asarray(list(res_scores_2.values()), np.float32)
-    # euclidean distance
-    dist = np.sqrt(np.sum((res_scores_1 - res_scores_2) ** 2))
-    sim = 1 - dist
-    # # L1 distance
-    # dist = np.sum(np.abs(res_scores_1 - res_scores_2))
-    # # Jaccard index
-    # min_sum = np.sum(np.minimum(res_scores_1, res_scores_2))
-    # max_sum = np.sum(np.maximum(res_scores_1, res_scores_2))
-    # sim = min_sum / max_sum
-    # dist = 1 - sim
+    
+    if distancetype == "euclidean":
+        dist = np.sqrt(np.sum((res_scores_1 - res_scores_2) ** 2))
+        sim = 1 - dist
+    if distancetype =="L1":
+        dist = np.sum(np.abs(res_scores_1 - res_scores_2))
+    if distancetype =="Jaccard":
+        min_sum = np.sum(np.minimum(res_scores_1, res_scores_2))
+        max_sum = np.sum(np.maximum(res_scores_1, res_scores_2))
+        sim = min_sum / max_sum
+        dist = 1 - sim
     
     return dist
 
 subset = all_binding_sites[:100]
-distance_matrix_scores = pairwise_distances_with_library(subset, residue_score_distance)
+distance_matrix_scores = pairwise_distances_with_library(subset, residue_score_distance, distancetype)
 
 # BAsed on distance vectors
 
@@ -104,7 +110,19 @@ distance_matrix_scores = pairwise_distances_with_library(subset, residue_score_d
 
 # Agglomerative
 
+def agglom_cluster(distance_matrix):
+    linkage_matrix = linkage(squareform(distance_matrix), method='average')
+    clusters = fcluster(linkage_matrix, t=0.5, criterion='distance')
+    agglom_model = AgglomerativeClustering(n_clusters=13, metric='precomputed', linkage='average')
+    cluster_labels = agglom_model.fit_predict(distance_matrix)
+    return cluster_labels
+
+
 # MeanShift
+def meanshift_cluster(distance_matrix):
+    mean_shift = MeanShift()
+    mean_shift_labels = mean_shift.fit_predict(distance_matrix)
+    return mean_shift_labels
 
 # DBSCAN
 
